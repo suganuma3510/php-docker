2c0e824e52804720fe52cee803d5f81bc2007b78
suganuma3510 <suganuma3510@gmail.com>
2021-06-10 06:49:49 +0900

    Add PDO driver

--- app/htdocs/index.php
+++ app/htdocs/index.php
@@ -1,2 +1,11 @@
 <?php
-print "Hello PHP";
+$dsn      = "mysql:host=db;dbname=php_sample;";
+$user     = 'root';
+$password = 'password';
+
+try {
+  $db = new PDO($dsn, $user, $password);
+  echo '接続成功';
+} catch (PDOException $e) {
+  echo '接続失敗' . $e->getMessage();
+}
