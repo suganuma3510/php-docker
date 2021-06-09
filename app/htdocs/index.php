d8725ff05b6cef6b958817f56cf46b953d6f3043
suganuma3510 <suganuma3510@gmail.com>
2021-06-10 08:03:40 +0900

    Fix PDO connection processing

--- app/htdocs/index.php
+++ app/htdocs/index.php
@@ -1,11 +1,11 @@
 <?php
-$dsn      = "mysql:host=db;dbname=php_sample;";
+$dsn      = 'mysql:host=db;dbname=php_sample;';
 $user     = 'root';
 $password = 'password';
 
 try {
   $db = new PDO($dsn, $user, $password);
-  echo '接続成功';
+  echo 'Hello PHP';
 } catch (PDOException $e) {
-  echo '接続失敗' . $e->getMessage();
+  echo 'DB接続失敗' . $e->getMessage();
 }
